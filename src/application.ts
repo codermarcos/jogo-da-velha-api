2b4b98249ceb47b7eac803b2ae04c970885d7ea4
codermarcos <coder.marcos@gmail.com>
2019-04-14 14:40:30 -0300

    :new: add logic to websocket

--- src/application.ts
+++ src/application.ts
@@ -6,28 +6,92 @@ import { IncomingMessage } from 'http';
 export default new class Application {
 
   private websocket = new WebSocket.Server({ port: 8080 }, this.start);
-  private games = new Object();
+  private games: { [key: string]: any } = new Object();
 
   constructor() {
-    this.websocket.on('connection', this.connection);
+    this.websocket.on('connection', this.connection.bind(this));
+  }
+
+  get hash(): string {
+    return Math.random().toString(36).substring(2, 10);
   }
 
   start() {
     console.log('application start');
   }
 
-  connection(_ws: WebSocket, req: IncomingMessage) {
+  connection(ws: WebSocket, req: IncomingMessage) {
+    const games = this.games;
     const { query } = Url(req.url || '', true);
+    const game = typeof query.game !== 'string' || query.game.length !== 8 ? this.hash.toString() : query.game;
+    const token = typeof query.token !== 'string' || query.token.length !== 8 ? this.hash.toString() : query.token;
 
-    typeof query.game === 'undefined'
-      && (query.game = Math.random().toString(36).substring(2, 15));
+    if (game in games) {
+      if (!('p2' in games[game])) {
+        ws.send(JSON.stringify({ game, token, event: 'waiting start' }));
+        ws.on('message', this.message.bind(this));
+        games[game].specs.push(ws);
 
-    Object.defineProperty(
-      this.games,
-      query.game,
-      {
-        value: new Game()
+        const args: IJogoDaVelhaJs = {
+          onstart() {
+            games[game].specs
+              .forEach(
+                (w: WebSocket) => w.send(JSON.stringify({ game, event: 'start' }))
+              );
+          },
+          onnext(data: INextEvent) {
+            games[game].specs
+              .forEach(
+                (w: WebSocket) => w.send(JSON.stringify({ game, data, event: 'next' }))
+              );
+          },
+          onfinish(data: string) {
+            games[game].specs
+              .forEach(
+                (w: WebSocket) => w.send(JSON.stringify({ game, data, event: 'finish' }))
+              );
+          }
+        }
+        games[game].game = new Game(args);
+        games[game].p2 = token;
+      } else {
+        ws.send(JSON.stringify({ game, event: 'specting' }));
+        ws.on('message', this.message.bind(this));
+        games[game].specs.push(ws);
       }
-    )
+    } else {
+      Object.defineProperty(games, game, {
+        value: {
+          specs: new Array(ws),
+          p1: token
+        }
+      });
+      ws.send(JSON.stringify({ game, token, event: 'waiting p2' }));
+      ws.on('message', this.message.bind(this));
+    }
+  }
+
+  play() {
+    
+  }
+
+  emit(specs: Array<WebSocket>) {
+    // specs
+    //   .forEach(
+
+    //   )
+  }
+
+  message(message: string) {
+    const data = JSON.parse(message);
+
+    switch (data.event) {
+      case 'play':
+        break;
+    
+      default:
+        console.log(data);
+        break;
+    }
   }
 }
